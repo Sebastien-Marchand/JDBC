e4e0605edd6fd583fac335e0bd5a198283d1005d
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-03-25 14:32:32 +0100

    1e version JDBC TP02

--- src/main/java/TPJDBC/TestConnexionJdbc.java
+++ src/main/java/TPJDBC/TestConnexionJdbc.java
@@ -5,15 +5,19 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
+        ResourceBundle db = ResourceBundle.getBundle("db");
+        
+        ResourceBundle db2 = ResourceBundle.getBundle("BDDCloud");
+		Class.forName(db2.getString("db.driver"));
 		
 		//créer la connexion a la bdd
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/compta","root","");
+		Connection connection = DriverManager.getConnection(db2.getString("db.url"), db2.getString("db.user"), db2.getString("db.pass"));
 		
 		//variable pour faire les requête
 		Statement statement = connection.createStatement();
@@ -31,7 +35,7 @@ public class TestConnexionJdbc {
         // étape 5 => libération des ressources
         resultSet.close();
 */
-		System.out.println(connection.getNetworkTimeout());
+		System.out.println(connection);
         statement.close();
         connection.close();
 
